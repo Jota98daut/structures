ee070bf20736f8537e81c5df7d1e108ee0a4bce3
Joel D'Autilio <joeldautilio@gmail.com>
2022-05-28 12:02:23 -0300

    Add List interface

---
+++ src/main/java/structures/List.java
@@ -0,0 +1,27 @@
+package structures;
+
+import java.util.Collection;
+
+/**
+ * List ADT representing a sequence of elements of type T.
+ * @author Joel D'Autilio
+ * @param T the type of objects the list can hold
+ */
+public interface List<T> extends Collection<T> {
+
+	/**
+	 * Add an element to a specified position of this list.
+	 * @param idx position where the element is to be added, in the range [0..n)
+	 * @param e non-null element to add
+	 * @return true iff the element was correctly added
+	 */
+	public boolean add(int idx, T e);
+
+	/**
+	 * Remove the element in a specified position of this list.
+	 * @param idx position where the element will be removed, in the range [0..n)
+	 * return the removed element
+	 */
+	public T remove(int idx);
+
+}
