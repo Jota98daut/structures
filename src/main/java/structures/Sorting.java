9a60e3989cdb1f372da8407bb62d116c3cca1ff8
Joel D'Autilio <joeldautilio@gmail.com>
2022-07-09 16:42:38 -0300

    Bubble sort

--- src/main/java/structures/Sorting.java
+++ src/main/java/structures/Sorting.java
@@ -10,6 +10,20 @@ public class Sorting {
 		}
 	}
 
+	public static void bubbleSort(Comparable[] a) {
+		int n = a.length;
+		boolean finished = false;
+		for(int last = n-1; last > 0 && !finished; last--) {
+			finished = true;
+			for(int i = 0; i < last; i++) {
+				if(a[i].compareTo(a[i+1]) > 0) { // a[i] > a[i+1]
+					swap(a, i, i+1);
+					finished = false;
+				} 
+			}
+		}
+	}
+
 	private static void swap(Object[] a, int i, int j) {
 		if(i < 0 || j >= a.length)
 			throw new IndexOutOfBoundsException("" + i + "," + j);
--- src/test/java/structures/SortingTest.java
+++ src/test/java/structures/SortingTest.java
@@ -14,9 +14,25 @@ public class SortingTest {
 	}
 
 	@Test
+	public void bubbleEmptyTest() {
+		Integer[] actual = new Integer[] {};
+		Sorting.bubbleSort(actual);
+		Integer[] expected = new Integer[] {};
+		assertArrayEquals("Empty array", expected, actual);
+	}
+
+	@Test
 	public void selectionUnitaryTest() {
 		Integer[] actual = new Integer[] {1};
 		Sorting.selectionSort(actual);
+		Integer[] expected = new Integer[] {1};
+		assertArrayEquals("Unitary array", expected, actual);
+	}
+
+	@Test
+	public void bubbleUnitaryTest() {
+		Integer[] actual = new Integer[] {1};
+		Sorting.bubbleSort(actual);
 		Integer[] expected = new Integer[] {1};
 		assertArrayEquals("Unitary array", expected, actual);
 	}
@@ -30,11 +46,31 @@ public class SortingTest {
 	}
 
 	@Test
+	public void bubbleSmallTest() {
+		Integer[] actual = new Integer[] {3,1,2};
+		Sorting.bubbleSort(actual);
+		Integer[] expected = new Integer[] {1,2,3};
+		assertArrayEquals("Small array", expected, actual);
+	}
+
+	@Test
 	public void selectionBigTest() {
 		Integer[] actual = new Integer[1000];
 		for(int i = 0; i < 1000; i++)
 			actual[i] = 999 - i;
 		Sorting.selectionSort(actual);
+		Integer[] expected = new Integer[1000];
+		for(int i = 0; i < 1000; i++)
+			expected[i] = i;
+		assertArrayEquals("Big array", expected, actual);
+	}
+
+	@Test
+	public void bubbleBigTest() {
+		Integer[] actual = new Integer[1000];
+		for(int i = 0; i < 1000; i++)
+			actual[i] = 999 - i;
+		Sorting.bubbleSort(actual);
 		Integer[] expected = new Integer[1000];
 		for(int i = 0; i < 1000; i++)
 			expected[i] = i;
